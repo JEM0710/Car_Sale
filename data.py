2fc0d99c7c140ea242753810d51c66c8bbfa344d
JEM0710 <122449582+JEM0710@users.noreply.github.com>
2023-01-19 02:27:43 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,144 @@
+list = {
+    'Hatchback': [
+        {
+            'Name': '2015 Kia Picanto',
+            'Transmission': 'Automatic',
+            'Odometer': '89k',
+            'Details': 'Front & rear dashcam, Touch screen head unit w/ 2 rear screen, Sound set-up',
+            'url' : 'https://i.postimg.cc/4xTHtgLs/Picanto.jpg'
+        },
+        {
+            'Name': '2019 Toyota Wigo G',
+            'Transmission': 'Manual',
+            'Odometer': '34k',
+            'Details': 'Fresh in & out',
+            'url' : 'https://i.postimg.cc/yxJK52sm/19WigoG.jpg'
+        },
+        {
+            'Name': '2019 Toyota Wigo TRD',
+            'Transmission': 'Automatic ',
+            'Odometer': '37k',
+            'Details': 'W/ navigation system, Fresh in & out',
+            'url' : 'https://i.postimg.cc/7LWwVxyH/19-Wigo-TRD.jpg'
+        }
+    ],
+    'Sedan': [
+        {
+            'Name': '2015 Toyota Vios J',
+            'Transmission': 'Manual',
+            'Odometer': '49k',
+            'Details': 'Upgraded engine, Full sound set-up, Touch screen head unit and mags',
+            'url' : 'https://i.postimg.cc/WzVc4tv4/15Vios.jpg'
+        },
+        {
+            'Name': '2014 Toyota Vios J',
+            'Transmission': 'Manual',
+            'Odometer': '102k',
+            'Details': 'All-powered, Front and rear dashcam, complete set-up',
+            'url' : 'https://i.postimg.cc/KzfhRxvr/14Vios.jpg'
+        },
+        {
+            'Name': '1999 Honda Civic LXi',
+            'Transmission': 'Manual',
+            'Odometer': '143k',
+            'Details': 'Carbon Hood, Loud exhaust, New mags & tires',
+            'url' : 'https://i.postimg.cc/fTmn9cLR/99Civic.jpg'
+        },
+        {
+            'Name': ' 2006 Mercedes Benz SLK 350',
+            'Transmission': 'Automatic',
+            'Odometer': '30k',
+            'Details': 'V6 engine, Hard top roof, Luxury car',
+            'url' : 'https://i.postimg.cc/g0RZ9BkP/06Slk.jpg'
+        }
+    ],
+    'Truck': [
+        {
+            'Name': '2014 Toyota Hilux G',
+            'Transmission': 'Automatic',
+            'Odometer': '105k',
+            'Details': 'Rollbar w/ luggage rack, Fender flares, Mags',
+            'url' : 'https://i.postimg.cc/sfHyCY6n/14Hilux.jpg'
+        },
+        {
+            'Name': '2008 Mitsubishi Strada GLX',
+            'Transmission': 'Manual',
+            'Odometer': '95k',
+            'Details': 'Mags, Fender Flares, Rollbar',
+            'url' : 'https://i.postimg.cc/7LQ6sF2V/08Strada.jpg'
+        },
+        {
+            'Name': '2013 Ford Ranger XLT',
+            'Transmission': 'Manual',
+            'Odometer': '130k',
+            'Details': 'Touch screen head unit, Mags',
+            'url' : 'https://i.postimg.cc/nz1WV2cX/13Ranger.jpg'
+        },
+        {
+            'Name': '2019 Nissan Navara EL',
+            'Transmission': 'Manual',
+            'Odometer': '51k',
+            'Details': 'Front & read bullbar, Rollbar w/ lights, Roofrack w/ luggage rack, Fender flares, Mags',
+            'url' : 'https://i.postimg.cc/7Ln9S6vc/19Navara.jpg'
+        },
+        {
+            'Name': '2015 Ford Ranger XLT',
+            'Transmission': 'Manual',
+            'Odometer': '43k',
+            'Details': 'Carryboy campershell, New mags & tires',
+            'url' : 'https://i.postimg.cc/tRYwPLk5/15Ranger.jpg'
+        }
+    ],
+    'SUV': [
+        {
+            'Name': '2015 Toyota Fortuner Black Series', 
+            'Transmission': 'Manual',
+            'Odometer': '97k',
+            'Details': 'Front dashcam, Fresh in & out',
+            'url' : 'https://i.postimg.cc/ZqMvdt73/15-Fortuner-BS.jpg'
+        },
+        {
+            'Name': '2017 Toyota Innova J',
+            'Transmission': 'Manual',
+            'Odometer': '69k',
+            'Details': 'All-powered, Fresh in & out',
+            'url' : 'https://i.postimg.cc/QdK23pL3/17Innova.jpg'
+        },
+        {
+            'Name': '2012 Toyota Fortuner V 4x4',
+            'Transmission': 'Automatic',
+            'Odometer': '85k',
+            'Details': 'New mags & tires, Fresh in & out',
+            'url' : 'https://i.postimg.cc/VsT4c2zJ/12-Fortuner.jpg'
+        },
+        {
+
+            'Name': '2011 Toyota Innova E',
+            'Transmission': 'Manual',
+            'Odometer': '81k',
+            'Details': 'New tires, Fresh in & out',
+            'url' : 'https://i.postimg.cc/7hGLr5wQ/245608140-137556041945230-8652986365943991661-n.jpg'
+        },
+        {
+            'Name': '2014 Mitsubishi Montero GLX',
+            'Transmission': 'Automatic',
+            'Odometer': '61k',
+            'Details': 'Topload carrier, Fender flares, Complete setup',
+            'url' : 'https://i.postimg.cc/4xtX5CxV/14-Montero-GLX.jpg'            
+        },
+        {
+            'Name': '2013 Mitsubishi Montero GLSV',
+            'Transmission': 'Automatic',
+            'Odometer': '91k',
+            'Details': 'Top of the line w/ paddle shift, Fresh in & out',
+            'url' : 'https://i.postimg.cc/W4hCngnK/13-Montero.jpg'            
+        },
+        {
+            'Name': '2016 Mitsubishi Adventure GLX',
+            'Transmission': 'Manual',
+            'Odometer': '39k',
+            'Details': 'New tires, Fresh in & out',
+            'url' : 'https://i.postimg.cc/jjDq7WJq/16-Adventure.jpg'           
+        }
+    ]
+}
